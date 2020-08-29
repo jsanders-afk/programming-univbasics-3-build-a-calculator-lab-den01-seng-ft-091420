7909d9a24df9355f94de062fd5334adcca42845a
Jack Sanders <guitarbreakerjs@gmail.com>
2020-08-29 08:56:29 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -9,18 +9,21 @@ def subtraction(num1, num2)
 end
 
 def division(num1, num2)
-  
-
+  div = num2 / num1
+  div
 end
 
 def multiplication(num1, num2)
-
+  mult = num1 * num2
+  mult
 end
 
 def modulo(num1, num2)
-
+  mod = num2 % num1
+  mod
 end
 
 def square_root(num)
+  
 
 end
